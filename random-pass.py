277b6117805a0b7e1f5538cf4c547157673c7e3f
abschill <ajay198810@gmail.com>
2022-05-17 18:20:42 -0600

    init

---
+++ random-pass.py
@@ -0,0 +1,8 @@
+import string
+
+from random import *
+
+chars = string.ascii_letters + string.punctuation + string.digits
+passw = ''.join(choice(chars) for x in range(randint(8,16)))
+
+print(passw)
