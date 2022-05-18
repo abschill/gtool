6e10445dc1d057ba7fccf4af99466a0614d534aa
abschill <ajay198810@gmail.com>
2022-05-18 07:29:10 -0600

    add bootstrapper for dotfiles

---
+++ bootstrap.py
@@ -0,0 +1,21 @@
+import requests, os
+# probably set this properly later
+p_path = os.getcwd()
+b_files = 'bootstrap'
+# global dotfiles regardless of proj type
+dotfiles = [
+    ['.editorconfig', 'https://raw.githubusercontent.com/abschill/html-chunk-loader/master/.editorconfig']
+]
+
+def ctxpath(rel, f):
+	if(f == None): return os.path.join(p_path, rel)
+	return os.path.join(p_path, rel, f)
+
+bs_path = os.listdir(ctxpath(b_files, ''))
+
+for file in bs_path:
+	f = open(ctxpath('bootstrap', file), 'r')
+	open(file, 'wb').write(f.read())
+for url in dotfiles:
+    r = requests.get(url[1], allow_redirects=True)
+    open(url[0], 'wb').write(r.content)
--- clean-exe.py
+++ clean-exe.py
@@ -1,8 +1,6 @@
 import os, subprocess
 
 splitter = filter(lambda fmap: len(fmap) < 2, list(map(lambda x: x.split('.'), os.listdir(os.getcwd()))))
-
-
 status = 0
 
 for fname in splitter:
