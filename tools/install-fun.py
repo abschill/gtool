ce4000e5f7cf27c4eed45c06f8fdc6ad6d652bfa
abschill <ajay198810@gmail.com>
2022-05-18 09:47:45 -0600

    add: ham

--- tools/install-fun.py
+++ tools/install-fun.py
@@ -4,6 +4,7 @@ status = 0
 
 status = run.bash('sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl')
 status = run.bash('sudo chmod a+rx /usr/local/bin/youtube-dl')
-status = run.bash('apt install rig')
-
+status = run.bash('sudo apt install rig')
+status = run.bash('sudo apt install 0ad')
+status = run.bash('sudo apt install fnt')
 print('Exit Code: ', status)
