d137f93979d335dcb0b3c3851be58782e22cc9ab
abschill <ajay198810@gmail.com>
2022-05-28 17:33:21 -0600

    mp4

--- tools/get-mp4.py
+++ tools/get-mp4.py
@@ -8,5 +8,5 @@ if len(sys.argv) < 2:
 	exit('error: no url to download. exit 1')
 
 # download with the merged audio + video
-cmd = 'youtube-dl -f \'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4\' ' + sys.argv[1]
+cmd = 'youtube-dl --recode-video mp4 ' + sys.argv[1]
 status = util.sh(cmd)
