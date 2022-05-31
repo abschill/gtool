57afe1d9c853dab8a0e10ad70236a93ccf4d181d
abschill <ajay198810@gmail.com>
2022-05-31 06:38:58 -0600

    update: darwin pkgs

--- install/mac-setup.zsh
+++ install/mac-setup.zsh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 
-brew install act actionlint aescrypt aescrypt-packetizer aespipe afflib afl-fuzz aften age agedu ahoy aircrack-ng alda algol68g analog antiword apib apidoc apktool arping as-tree asio austin autobench autoconf autoenv automake autogen autorest tmux m-cli httpie fselect bat asciinema ack trash macSVG slack youtube-dl ffmpeg 
+brew install act actionlint aescrypt aescrypt-packetizer aespipe afflib afl-fuzz aften age agedu ahoy aircrack-ng alda algol68g analog antiword apib apidoc apktool arping as-tree asio austin autobench autoconf autoenv automake autogen autorest tmux m-cli httpie fselect bat asciinema ack trash macSVG slack youtube-dl ffmpeg cmake clang-format john wireshark binutils radare2 htop wget nmap lynx geoiplookup watch irssi
