7097fa2735c10f717148124a5d159c58fdbdfa18
abschill <ajay198810@gmail.com>
2022-06-05 08:16:25 -0600

    chore: updates

--- tools/get-pips.py
+++ tools/get-pips.py
@@ -4,7 +4,25 @@ status = 0
 pips = [
 	'pyyaml',
 	'numpy',
-	'Cython'
+	'cython',
+	'opencv',
+	'pyqt',
+	'pywin32',
+	'pytest',
+	'pendulum',
+	'plotly',
+	'requests',
+	'keras',
+	'bokeh',
+	'tkinter',
+	'moviepy',
+	'pygame',
+	'chardet',
+	'tqdm',
+	'ipython',
+	'scikit-learn',
+	'pytorch',
+	'scipy'
 ]
 for i in pips:
 	status = util.pip(('install', i))
---
+++ tools/get-playlist.sh
@@ -0,0 +1,1 @@
+youtube-dl -i $1
